748af2f7d2a73ee2e33edac98466c43bfb5a2537
kauan izar <kauanizar02@gmail.com>
2023-12-02 18:39:41 -0300

    amor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿Console.WriteLine("---- Entrada Válida ----\n");
+
+int numeroDigitado = -1;
+
+while (numeroDigitado  < 0 || numeroDigitado > 9)
+{
+Console.Write("Digite um númer entre 1 e 9 (ou 0 pra cancelar)...:");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numeroDigitado == 0)
+{
+Console.WriteLine("\nOperação cancelada.");    
+}
+else
+{
+Console.WriteLine($"\nNúmero selecionado = {numeroDigitado}");    
+}
+
+Console.WriteLine("\nVolte sempre!");
